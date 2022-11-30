0c156aae376884e2a4ec0c9c3f59e2bd1c4a16cc
Pedro Ramalho <pedro_hrr@hotmail.com>
2022-11-30 03:20:37 -0300

    first commit

---
+++ src/test/java/com/Microservices/Microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Microservices.Microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
